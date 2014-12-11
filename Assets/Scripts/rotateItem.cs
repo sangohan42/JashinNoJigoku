d90027713a20425b73bb7d4522f10ffb4556fec2
Contamine Cyrille <cyrille@goodia.co.jp>
2014-12-11 12:58:10 +0900

    Add script to rotate item

---
+++ Assets/Scripts/rotateItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateItem : MonoBehaviour {
+
+	private float degreesPerSecond;
+	// Use this for initialization
+	void Start () {
+		degreesPerSecond = 120;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up* degreesPerSecond*Time.deltaTime, Space.Self); 
+	}
+}
