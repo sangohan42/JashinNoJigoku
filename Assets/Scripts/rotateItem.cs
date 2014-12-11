f2baf04cb703d857b4f901f758f9eff9bfb5a416
Contamine Cyrille <cyrille@goodia.co.jp>
2014-12-11 15:23:36 +0900

    Add items and collider + scripts in scene

--- Assets/Scripts/rotateItem.cs
+++ Assets/Scripts/rotateItem.cs
@@ -6,11 +6,20 @@ public class rotateItem : MonoBehaviour {
 	private float degreesPerSecond;
 	// Use this for initialization
 	void Start () {
-		degreesPerSecond = 120;
+		degreesPerSecond = 160;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate(Vector3.up* degreesPerSecond*Time.deltaTime, Space.Self); 
 	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag(DoneTags.player))
+		{
+			Destroy (this.gameObject);
+		}
+	}
 }
+
