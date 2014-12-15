df0e9d340617cccb7ab833f45e81e4624f8b8755
futohashi <chicago_bulls_eye@hotmail.co.jp>
2014-12-15 18:51:29 +0900

    Add game clear game over scene.

---
+++ Assets/Scripts/UI/UIGameOver.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIGameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnPress(bool isDown)
+	{
+		switch(gameObject.name)
+		{
+			case "RetryLabel":
+				FadeManager.Instance.LoadLevel("testScene", 0.25f);		
+			break;
+		}
+	}
+}
